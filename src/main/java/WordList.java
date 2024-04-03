3bdd8a4a503489aa9ac752c504739414594d84f7
nat3chan <142937438+nat3chan@users.noreply.github.com>
2024-04-03 12:18:12 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -19,7 +19,7 @@ public class WordList
 
   public void removeWordsOfLength(int len)
   {
-    for(int i = myList.size(); i >= 0; i--)
+    for(int i = myList.size()-1; i >= 0; i--)
       if(myList.get(i).length() == len)
         myList.remove(i);
   }
